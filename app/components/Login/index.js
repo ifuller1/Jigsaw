49d63721c6138956bf2b32671cb355a497999acf
Ian Fuller <ian.fuller@trailsuite.com>
2016-07-18 22:07:19 +0100

    Added Login route / page

---
+++ app/components/Login/index.js
@@ -0,0 +1,18 @@
+/**
+*
+* Login
+*
+*/
+
+import React from 'react';
+
+import styles from './styles.css';
+
+function Login() {
+  return (
+    <div className={styles.login}>
+    </div>
+  );
+}
+
+export default Login;
--- app/routes.js
+++ app/routes.js
@@ -34,7 +34,15 @@ export default function createRoutes() {
         importModules.catch(errorLoading);
       },
     }, {
+      path: '/login',
+      getComponent(location, cb) {
+        System.import('components/Login')
+          .then(loadModule(cb))
+          .catch(errorLoading);
+      },
+    }, {
       path: '*',
+
       name: 'notfound',
       getComponent(nextState, cb) {
         System.import('containers/NotFoundPage')
